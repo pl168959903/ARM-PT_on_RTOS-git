c4ffd7de62d696f36e7f274f019e7f392e290a6e
ZGR <pl168959903@gmail.com>
2020-02-10 07:15:45 +0800

    20200210

--- Libraries/User/src/CJ_OV528.c
+++ Libraries/User/src/CJ_OV528.c
@@ -29,8 +29,7 @@ void OV528_ErrHook( uint8_t err ) {
         break;
     }
 
-    if ( err - 1 )
-        printf( "Camena err!!	ERR CODE : %X\n", err );
+    if ( err - 1 ) printf( "Camena err!!	ERR CODE : %X\n", err );
     // while(1) {};
 }
 
@@ -116,8 +115,7 @@ uint8_t OV528_GetPictue( OV528_T* OV528, uint8_t imageType ) {
     // check ack
     Array_assign( OV528->RS232_CMD, 6, 0xAA, OV528_CMD_ID_ACK, OV528_CMD_ID_GET_PICTURE, 0x00, 0x00, 0x00 );
     cmdCheck = FIFO_cmdCheck( OV528->buf, OV528->RS232_CMD, 6, 250 );
-    if ( !cmdCheck )
-        goto ERR;
+    if ( !cmdCheck ) goto ERR;
 
     // check image config
     Array_assign( OV528->RS232_CMD, 3, 0xAA, OV528_CMD_ID_DATA, OV528->imageType );
@@ -144,8 +142,7 @@ Exit:
     OV528->imageType   = imageType;
     OV528->imageSize   = ( uint32_t )( FIFO_read_byte( OV528->buf, 9 ) << 0 ) | ( uint32_t )( FIFO_read_byte( OV528->buf, 10 ) << 8 ) | ( uint32_t )( FIFO_read_byte( OV528->buf, 11 ) << 16 );
     OV528->imagePacket = OV528->imageSize / ( OV528->packetSize - 6 );
-    if ( OV528->imageSize % ( OV528->packetSize - 6 ) )
-        OV528->imagePacket++;
+    if ( OV528->imageSize % ( OV528->packetSize - 6 ) ) OV528->imagePacket++;
 
     FIFO_reset( OV528->buf );
     OV528_DELAY_MS( 1 );
@@ -269,7 +266,7 @@ uint8_t OV528_SetPacketSize( OV528_T* OV528, uint16_t size ) {
     FIFO_reset( OV528->buf );
     OV528->writeData( OV528->RS232_CMD, 6 );
 
-    //check ack
+    // check ack
     Array_assign( OV528->RS232_CMD, 6, 0xAA, OV528_CMD_ID_ACK, OV528_CMD_ID_SET_PACKAGE_SIZE, 0x00, 0x00, 0x00 );
     cmdCheck = FIFO_cmdCheck( OV528->buf, OV528->RS232_CMD, 6, 250 );
     if ( cmdCheck )
@@ -311,29 +308,38 @@ uint16_t OV528_GetPacket( OV528_T* OV528, uint16_t package_ID, uint8_t package[]
     OV528->writeData( OV528->RS232_CMD, 6 );
 
     // check ack
-    Array_assign( OV528->RS232_CMD, 2, 0xAA, ID[ 0 ], ID[ 1 ] );
-    if ( FIFO_waitData( OV528->buf, 4, 250 ) )
-        if ( ( FIFO_read_byte( OV528->buf, 0 ) == ID[ 0 ] ) && ( FIFO_read_byte( OV528->buf, 1 ) == ID[ 1 ] ) ) {
-            size = ( uint16_t )OV528->buf->buf[ 2 ] | ( uint16_t )( OV528->buf->buf[ 3 ] << 8 );
-            if ( FIFO_waitData( OV528->buf, size + 6, 3000 ) ) {
-                for ( i = 0; i < size + 4; i++ )
-                    checkSum += OV528->buf->buf[ i ];
-                if ( ( ( uint8_t )( checkSum & 0x00ff ) == OV528->buf->buf[ size + 4 ] ) && ( ( uint8_t )( checkSum >> 8 ) == OV528->buf->buf[ size + 5 ] ) ) {
-                    for ( i = 0; i < size; i++ )
-                        package[ i ] = OV528->buf->buf[ i + 4 ];
-                    goto Exit;
-                }
+    Array_assign( OV528->RS232_CMD, 3, 0xAA, ID[ 0 ], ID[ 1 ] );
+    cmdCheck = FIFO_cmdCheck( OV528->buf, OV528->RS232_CMD, 3, 250 );
+    if ( !cmdCheck ) goto ERR;
+
+    cmdCheck = FIFO_waitData( OV528->buf, 4, 250 );
+    if ( !cmdCheck ) goto ERR;
+    
+    if ( ( FIFO_read_byte( OV528->buf, 0 ) == ID[ 0 ] ) && ( FIFO_read_byte( OV528->buf, 1 ) == ID[ 1 ] ) ) {
+        size = ( uint16_t )OV528->buf->buf[ 2 ] | ( uint16_t )( OV528->buf->buf[ 3 ] << 8 );
+        if ( FIFO_waitData( OV528->buf, size + 6, 3000 ) ) {
+            for ( i = 0; i < size + 4; i++ )
+                checkSum += OV528->buf->buf[ i ];
+            if ( ( ( uint8_t )( checkSum & 0x00ff ) == OV528->buf->buf[ size + 4 ] ) && ( ( uint8_t )( checkSum >> 8 ) == OV528->buf->buf[ size + 5 ] ) ) {
+                for ( i = 0; i < size; i++ )
+                    package[ i ] = OV528->buf->buf[ i + 4 ];
+                goto Exit;
             }
         }
-    goto ERR;
+        goto ERR;
 
-ERR:
-    OV528_ErrHook( OV528_ERR_GETPACKET );
-    FIFO_reset( OV528->buf );
-    return 0;
+    ERR :
+#if OV528_USE_ERR_HOOK
+    {
+        OV528_ErrHook( OV528_ERR_GETPACKET );
+    }
+#endif
+        FIFO_reset( OV528->buf );
+        OV528_DELAY_MS( 1 );
+        return 0;
 
-Exit:
-    FIFO_reset( OV528->buf );
-    OV528_CMD_DELAY();
-    return size;
-}
+    Exit:
+        FIFO_reset( OV528->buf );
+        OV528_DELAY_MS( 1 );
+        return size;
+    }
