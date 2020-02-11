a81c67c996835aec63e1e8629e3803fea766a34e
ZGR <pl168959903@gmail.com>
2020-02-11 16:48:28 +0800

    20200211

--- main.c
+++ main.c
@@ -20,9 +20,6 @@ int main( void ) {
                     ( void* )           NULL, 
                     ( UBaseType_t )     1, 
                     ( TaskHandle_t* )   &taskBootCreate_handler 
-
-
-                    
                 );
 
     vTaskStartScheduler();
